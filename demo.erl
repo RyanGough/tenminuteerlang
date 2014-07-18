f31fe8902dfad3bc95db37aba198f12833f3d3cf
Ryan Gough <ryan.gough@gmail.com>
2014-07-18 23:20:09 +0100

    demo 2 - a supervisor

--- demo.erl
+++ demo.erl
@@ -1,11 +1,23 @@
 -module (demo).
 
--export ([helloworld/0]).
+-export ([helloworld/0,supervisor/0]).
+
+supervisor() ->
+    process_flag(trap_exit, true),
+    register(demo, spawn_link(demo, helloworld, [])),
+    receive
+        {'EXIT', _From, Reason} ->
+            io:format("crashed because: ~p~n", [Reason]),
+            supervisor()
+    end.
 
 helloworld() ->
     receive
         hello ->
             io:format("hello, world!~n"),
+            helloworld();
+        {divide, Dividend, Divisor} ->
+            io:format("the result is: ~p~n", [Dividend / Divisor]),
             helloworld()
     end.
     
